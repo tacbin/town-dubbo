92c1d9a2368aa80d039c8c121c416460a41a891c
Administrator <haha>
2020-05-03 00:53:40 +0800

    配置mysql

--- weixin-web/src/main/java/com/tacbin/weixin/web/WeixinWebApplication.java
+++ weixin-web/src/main/java/com/tacbin/weixin/web/WeixinWebApplication.java
@@ -1,9 +1,11 @@
 package com.tacbin.weixin.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({"com.tacbin.framework.data.mapper"})
 public class WeixinWebApplication {
 
     public static void main(String[] args) {
