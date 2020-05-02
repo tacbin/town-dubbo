43d6ffe7730e785ad43b5cd06e97c92a1cec5151
Administrator <haha>
2020-05-03 01:39:59 +0800

    微信验证

--- weixin-web/src/main/java/com/tacbin/weixin/web/controller/TokenValidController.java
+++ weixin-web/src/main/java/com/tacbin/weixin/web/controller/TokenValidController.java
@@ -1,22 +1,46 @@
 package com.tacbin.weixin.web.controller;
 
-import com.tacbin.framework.base.common.ResponseInfo;
-import com.tacbin.framework.base.common.Status;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Description :
  * @Author : Administrator
  * @Date : 2020-05-02 23:31
  **/
+@Slf4j
 @RestController
+@AllArgsConstructor
 @RequestMapping("/wx")
 public class TokenValidController {
+    private final WxMpService wxService;
+    private final WxMpMessageRouter messageRouter;
 
-    @RequestMapping(path = "/valid", method = {RequestMethod.GET, RequestMethod.POST})
-    public ResponseInfo<String> tokenValid() {
-        return new ResponseInfo<>("校验成功", Status.SUCCESS, "success");
+    @GetMapping(produces = "text/plain;charset=utf-8")
+    public String authGet(@PathVariable String appid,
+                          @RequestParam(name = "signature", required = false) String signature,
+                          @RequestParam(name = "timestamp", required = false) String timestamp,
+                          @RequestParam(name = "nonce", required = false) String nonce,
+                          @RequestParam(name = "echostr", required = false) String echostr) {
+
+        log.info("\n接收到来自微信服务器的认证消息：[{}, {}, {}, {}]", signature,
+                timestamp, nonce, echostr);
+        if (StringUtils.isAnyBlank(signature, timestamp, nonce, echostr)) {
+            throw new IllegalArgumentException("请求参数非法，请核实!");
+        }
+
+        if (!this.wxService.switchover(appid)) {
+            throw new IllegalArgumentException(String.format("未找到对应appid=[%s]的配置，请核实！", appid));
+        }
+
+        if (wxService.checkSignature(timestamp, nonce, signature)) {
+            return echostr;
+        }
+
+        return "非法请求";
     }
 }
