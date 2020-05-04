e8b9a128f46fd42c70062af4b7babb269ac51b06
Administrator <haha>
2020-05-04 10:43:15 +0800

    群发测试

--- weixin-web/src/main/java/com/tacbin/weixin/web/controller/message/MassMessageController.java
+++ weixin-web/src/main/java/com/tacbin/weixin/web/controller/message/MassMessageController.java
@@ -4,14 +4,19 @@ import com.tacbin.framework.base.common.ResponseInfo;
 import com.tacbin.framework.base.common.Status;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpMassMessageService;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.WxMpMassNews;
+import me.chanjar.weixin.mp.bean.WxMpMassOpenIdsMessage;
+import me.chanjar.weixin.mp.bean.result.WxMpMassSendResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 
 /**
  * @Description :
@@ -25,15 +30,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class MassMessageController {
     private WxMpService mpService;
 
-    @RequestMapping(path = "/upload-news", method = RequestMethod.POST)
-    public ResponseInfo<String> uploadNews(@RequestBody WxMpMassNews news) {
-        WxMpMassMessageService massMessageService = mpService.getMassMessageService();
-        try {
-            massMessageService.massNewsUpload(news);
-            return new ResponseInfo<>("success", Status.SUCCESS, null);
-        } catch (WxErrorException e) {
-            log.error("upload failed:{}", e.getMessage());
-            return new ResponseInfo<>("上传失败", Status.FAIL, e.getMessage());
-        }
+    @RequestMapping(path = "/sendText", method = RequestMethod.POST)
+    public void sendText(String openid) throws WxErrorException {
+        // 发送群发消息
+        WxMpMassOpenIdsMessage massMessage = new WxMpMassOpenIdsMessage();
+        massMessage.setMsgType(WxConsts.MassMsgType.TEXT);
+        massMessage.setContent("测试群发消息\n欢迎欢迎，热烈欢迎\n换行测试\n超链接:<a href=\"http://www.baidu.com\">Hello World</a>");
+        massMessage.getToUsers().add(openid);
+        WxMpMassSendResult massResult = this.mpService.getMassMessageService()
+                .massOpenIdsMessageSend(massMessage);
+        assertNotNull(massResult);
+        assertNotNull(massResult.getMsgId());
     }
 }
