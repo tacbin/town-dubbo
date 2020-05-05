adf87b1fabf0b6fc855ea335e02662e057128c61
Administrator <haha>
2020-05-05 11:32:39 +0800

    自定义过滤器

---
+++ weixin-web/src/main/java/com/tacbin/weixin/web/config/filter/AuthcFilter.java
@@ -0,0 +1,34 @@
+package com.tacbin.weixin.web.config.filter;
+
+import com.tacbin.weixin.common.utils.JsonUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * @Description :web过滤器
+ * @Author : Administrator
+ * @Date : 2020-05-05 11:23
+ **/
+@Component
+@WebFilter(filterName = "authcFilter", urlPatterns = "/*")
+@Slf4j
+public class AuthcFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("请求:{}", JsonUtils.toJson(servletRequest));
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
