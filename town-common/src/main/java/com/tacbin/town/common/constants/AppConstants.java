1db983b396d35d65dfe109fb7e526ef62352f217
Administrator <haha>
2020-07-16 20:01:25 +0800

    修改图片源

--- town-common/src/main/java/com/tacbin/town/common/constants/AppConstants.java
+++ town-common/src/main/java/com/tacbin/town/common/constants/AppConstants.java
@@ -15,7 +15,7 @@ public class AppConstants {
 
     /** 接口配置信息校验的aesKey */
     public static final String ENCODING_AES_KEY = "HoR6W3oTj1lc6UQ64AouwMGDlOFKGCXsbYIh0dPaDro";
-    public static final String APP_ID = "wx01a255b907e83392";
+    public static final String APP_ID = "wx596f8629885ab81f";
     public static final String SECRET_ID = "1899e5003c8e47bad546d8998df87594";
     public static final String ENCODING_AES = "aes";
 
