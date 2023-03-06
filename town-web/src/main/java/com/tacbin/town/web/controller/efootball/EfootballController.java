9265f8162d0491b80f6f10735ab8bbc7f25102b3
Weibin Liu <weibin.liu@shopee.com>
2023-03-06 11:31:38 +0800

    1

--- town-web/src/main/java/com/tacbin/town/web/controller/efootball/EfootballController.java
+++ town-web/src/main/java/com/tacbin/town/web/controller/efootball/EfootballController.java
@@ -1,15 +1,8 @@
 package com.tacbin.town.web.controller.efootball;
 
 
-import com.sun.xml.internal.ws.resources.UtilMessages;
-import com.tacbin.town.api.service.entity.ProductDataEntity;
-import com.tacbin.town.common.constants.RedisConstants;
 import com.tacbin.town.common.entity.ResponseInfo;
 import com.tacbin.town.common.entity.Status;
-import com.tacbin.town.common.utils.SnowFlakeUtil;
-import com.tacbin.town.web.util.UserAgentUtils;
-import com.tacbin.town.web.util.ip.AddressUtils;
-import cz.mallat.uasparser.UserAgentInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.params.SetParams;
 
 import javax.annotation.Resource;
 import java.util.UUID;
